a4916902f8e4f9f609e4551b62e256a53d6ba591
Reeve Jarvis <reevejarvis@hotmail.com>
2021-12-04 18:43:04 -0800

    Create image upload handling and validation functions

--- upload_image.php
+++ upload_image.php
@@ -17,3 +17,49 @@
     define("FILE_TOO_LARGE", "<div><h3>The file is too large to upload.</h3></div>");
     define("INVALID_FORMAT", "<div><h3>File is not the correct format.</h3></div>");
 
+    function isImage($file)
+    {
+        $check = getimagesize($file["tmp_name"]);
+        return ($check !== false) ? true : false;
+    }
+
+    function fileAlreadyExists($file)
+    {
+        return (file_exists($file)) ? true : false;
+    }
+
+    function isFileSizeOk($file)
+    {
+        return ($file["size"] > 500000) ? false : true;
+    }
+
+    function isImageFormatOk($file_type)
+    {
+        $acceptedFormats = ["jpg", "png", "jpeg", "gif"];
+        return (in_array($file_type, $acceptedFormats)) ? true : false;
+    }
+
+    function handleImageUpload($upload, $target_image_file, $image_file_type)
+    {
+        if (isImage($upload)) {
+            if (!fileAlreadyExists($target_image_file)) {
+                if (isFileSizeOk($upload)) {
+                    if (isImageFormatOk($image_file_type)) {
+                        if (move_uploaded_file($upload["tmp_name"], $target_image_file)) {
+                            echo "<div><p>The file " . htmlspecialchars(basename($upload["name"])) . " has been uploaded.</p></div>";
+                        } else {
+                            echo FAILED_UPLOAD;
+                        }
+                    } else {
+                        echo INVALID_FORMAT;
+                    }
+                } else {
+                    echo FILE_TOO_LARGE;
+                }
+            } else {
+                echo DUPLICATE_FILE;
+            }
+        } else {
+            echo INVALID_FILE;
+        }
+    }
