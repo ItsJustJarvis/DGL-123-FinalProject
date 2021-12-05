e963ef76ed8f5b966f11eb564a6b9d0bc82a110b
Reeve Jarvis <reevejarvis@hotmail.com>
2021-12-04 18:40:13 -0800

    Connect to database

--- add_character.php
+++ add_character.php
@@ -5,3 +5,6 @@ define("INVALID_DATA", "<div><p>That is not a valid entry.</p></div>");
 define("SUCCESS", "<div><p>Character successfully uploaded to database. Please refresh the page.</p></div>");
 define("FAILURE", "<div><p>Character upload failed.</p></div>");
 define("ALREADY_EXISTS", "<div><p>Character already in database.</p></div>");
+
+$conn = mysqli_connect('localhost', 'root', '', 'simpsons_archive') or die("Error " . mysqli_error($conn));
+
